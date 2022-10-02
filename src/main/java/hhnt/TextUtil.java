807265b5e6ab73b656a40dc00b310fb02b40dfdf
hieund7 <ngohieutp@gmail.com>
2022-10-02 11:18:42 +0700

    Second commit

--- src/main/java/hhnt/TextUtil.java
+++ src/main/java/hhnt/TextUtil.java
@@ -3,8 +3,7 @@ package hhnt;
 public class TextUtil {
 
     public static String trim(String text) {
-        String textTrim = text.trim();
-        return textTrim;
+        return text.trim();
     }
 
 }
